89fc666ee82bdd2703ad03054aeda83302336bec
José Lopes Neto <jose.neto@taxweb.com.br>
2015-06-23 11:45:38 -0300

    fix to work with mongoid

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -4,9 +4,7 @@ module ProgressJob
     def show
       @delayed_job = Delayed::Job.find(params[:job_id])
       if @delayed_job
-        max = (@delayed_job[:progress_max].to_f || 100)
-        percentage = !max.zero? ? @delayed_job[:progress_current] / max * 100 : 0
-        render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+        render :json, { id: @delayed_job.id, progress_stage: @delayed_job[:progress_stage] , progress_max: @delayed_job[:progress_max], progress_current: @delayed_job[:progress_current] }
       else
         # render :status => 404
         raise "job id #{params[:job_id]} not found"
